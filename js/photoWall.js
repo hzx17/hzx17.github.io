061915e01c3c3cfe1328627d327d34e790f21539
hzx17 <2116845913@qq.com>
2022-06-30 13:27:44 +0800

    Site updated: 2022-06-30 13:27:43

--- js/photoWall.js
+++ js/photoWall.js
@@ -1,5 +1,5 @@
 var imgDataPath = "/photos/photos.json"; //图片名称高宽信息json文件路径
-var imgPath = "http://rcj536al9.hn-bkt.clouddn.com/"; //图片访问路径
+var imgPath = "http://imghe.xyz/"; //图片访问路径
 var imgMaxNum = 50; //图片显示数量
 
 var windowWidth =
